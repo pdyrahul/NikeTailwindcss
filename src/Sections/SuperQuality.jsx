3294d2cef62d86911c3f2ad93ae9f0c750e57c62
rahul_git <rahulpdy.pandey@gmail.com>
2024-07-14 23:37:43 +0530

    spaceadd

--- src/Sections/SuperQuality.jsx
+++ src/Sections/SuperQuality.jsx
@@ -6,7 +6,7 @@ const SuperQuality = () => {
       <div className=" flex flex-1 flex-col">
         <h2 className="font-palanquin text-4xl  capitalize lg:max-w-lg font-bold">
           We Provide You <br />
-           <span className="text-coral-red ">Super Quality</span>Shoes
+           <span className="text-coral-red "> Super Quality </span>Shoes
         </h2>
         <p className="font-montserrat mt-4 info-text lg:max-w-lg">
           Ensuring Premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.
